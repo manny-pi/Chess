e539e7ef831d0c675725123b7509488f4df45378
Prince Okpoziakpo <48536998+manny-pi@users.noreply.github.com>
2021-09-21 17:35:41 +0000

    # Made changes to the enemies * Game now supports multiple enemies at once * Changed the way object speeds are updated

--- ShooterDemo/GUI/shooterGUI.py
+++ ShooterDemo/GUI/shooterGUI.py
@@ -1,9 +1,10 @@
-import _SpriteBodies.spritebodies as spritebody
 import pygame
 import pygame.mixer 
+import pygame.time 
 from pygame.locals import * 
-import random
-
+import _SpriteBodies.spritebodies as spritebody
+import time 
+from random import randint as r 
 
 class Shooter(spritebody.PlayerSprite):
     SHOOTER_IMAGE = pygame.image.load("./ShooterDemo/images/shooter.png")
@@ -35,16 +36,18 @@ class Shooter(spritebody.PlayerSprite):
 class Enemy(spritebody.EnemySprite):
     ENEMY_IMAGE = pygame.image.load("./ShooterDemo/images/enemy.png")
     ENEMY_IMAGE = pygame.transform.scale(ENEMY_IMAGE, (60, 20))
+    SPEED = 5 
 
-    def __init__(self, init_x=0, init_y=0, speed=8):
-        super().__init__(init_x, init_y, speed, img=Enemy.ENEMY_IMAGE)
+    def __init__(self, init_x=0, init_y=0):
+        super().__init__(init_x, init_y, speed=Enemy.SPEED, img=Enemy.ENEMY_IMAGE)
 
     def update(self):
         self.rect.move_ip(-self.speed, 0)
 
-    def speedUp(self):
-        if self.speed < 26:
-            self.speed += 2
+    @classmethod
+    def speedUp(cls):
+        if cls.SPEED <= 20:
+            cls.SPEED += 3
 
 
 class Bullet(spritebody.OtherSprite):
@@ -59,11 +62,6 @@ class Bullet(spritebody.OtherSprite):
         self.rect.move_ip(self.speed, 0)
         if self.rect.right > 800:
             self.kill()
-
-    @staticmethod
-    def speedUp():
-        if Bullet.SPEED < 15:
-            Bullet.SPEED += 1
 
 
 class ShooterGUI:
@@ -82,6 +80,9 @@ class ShooterGUI:
     WINDOW_HEIGHT = 400
     WINDOW_BACKGROUND = pygame.image.load("./ShooterDemo/images/background.png")
     
+    ENEMIES_LIMIT = 10 
+    LEVEL = 0 
+
     # GAME AUDIO 
     filename = "./ShooterDemo/audio/shooteraudio.ogg"
     SHOOTER_SOUND = pygame.mixer.Sound(filename)
@@ -89,10 +90,10 @@ class ShooterGUI:
     # SHOOTER VARIABLES
     # - - - - - - - - - - - - - - - - - -
     PLAYER_SCORE = 0
-    LEVEL = 0 
     
 
     def __init__(self):
+        # Create the game window 
         self.game_window = pygame.display.set_mode((800, 400), display=0)
 
         # Create the game sprites 
@@ -133,54 +134,56 @@ class ShooterGUI:
                         bullet = self.player.shoot()
                         self.bullets.add(bullet)
 
-
-                        enemy = Enemy(800, random.randint(0, 400))
-                        self.enemies.add(enemy)
-
                         # Play audio for shooting
                         # REVIEW: Not working :( 
                         ShooterGUI.SHOOTER_SOUND.play() 
-                        # print("Audio played")
                         
 
             # Update Sprite locations / Handle Sprite collisions / Update player score
             # - - - - - - - - - - - - - - - - - -
             # Returns the state of all the keys in the keyboard in array; use to update Shooter sprite location
             pressed_keys = pygame.key.get_pressed()
-
             # Update Shooter sprite location, and draw Shooter sprite
             self.player.update(pressed_keys, ShooterGUI.WINDOW_WIDTH, ShooterGUI.WINDOW_HEIGHT)
+
+            # Returns the number of milliseconds that have passed / Create Enemy sprite every 2 seconds
+            secondsPassed = int(pygame.time.get_ticks() / 1000) 
+            if secondsPassed % 2 == 0 and len(self.enemies) < ShooterGUI.ENEMIES_LIMIT: 
+              enemy = Enemy(self.WINDOW_WIDTH, r(0, self.WINDOW_HEIGHT))
+              self.enemies.add(enemy)
 
             # Remove sprites that collide from their groups
             bullets_and_enemies = pygame.sprite.groupcollide(self.bullets, self.enemies, dokilla=True, dokillb=True)
 
-            # Update the player score; Change the mechanism 
+            # Update the player score
+            # REVIEW: Need to make a more sensible method for increasing the score 
             if len(bullets_and_enemies) != 0: 
               ShooterGUI.PLAYER_SCORE += 1
               
+            # Increase the speed of Enemy sprites / Display next level prompt 
+            if ShooterGUI.PLAYER_SCORE % 10 == 0 and ShooterGUI.PLAYER_SCORE != 0: 
+              Enemy.speedUp()  
+
 
             # Draw graphics to the window 
             # - - - - - - - - - - - - - - - - - -
             # Draw background
             self.game_window.blit(ShooterGUI.WINDOW_BACKGROUND, (0, 0))
 
+            # Draw the Shooter sprite on the game window
+            self.game_window.blit(self.player.surface, self.player.rect)   
+
             # Draw Bullet sprites on the game window
             for bullet in self.bullets:
                 bullet.update()
-          
                 self.game_window.blit(bullet.surface, bullet.rect)
 
             # Draw the Enemy sprites on the game window 
             for enemy in self.enemies: 
               enemy.update() 
-              self.game_window.blit(enemy.surface, enemy.rect)
+              self.game_window.blit(enemy.surface, enemy.rect)     
 
-            # Update the score
-            self.game_window.blit(self.player.surface, self.player.rect)
-            # self.game_window.blit(self.enemy.surface, self.enemy.rect)
-        
-
-            # Display the score on the screen
+            # Display the player score on the screen
             self.game_window.blit(ShooterGUI.get_score_text(ShooterGUI.PLAYER_SCORE)
                                   , (700, 10))
 
